662c63c940d47489658a4306f7ad3bd2c84aaa78
Aleksey Sokolov <operator_nss@mail.ru>
2022-10-18 19:16:25 +0300

    edit answer

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import React, {FC, useCallback, useEffect, useState} from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { setSelectAnswer } from '../../redux/slices/QuestionSlice';
+import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
+import {setSelectAnswer} from '../../redux/slices/QuestionSlice';
 import IconArrow from '../IconArrow/IconArrow';
 import IconWrong from '../IconWrong/IconWrong';
 
@@ -11,18 +11,18 @@ type AnswerProps = {
   rightAnswers: string[] | undefined;
 };
 
-const Answer: FC<AnswerProps> = ({ title, rightAnswers, answered }) => {
+const Answer: FC<AnswerProps> = ({title, rightAnswers, answered}) => {
   const [selected, setSelected] = useState<boolean>(false);
   const [wrongIcon, setWrongIcon] = useState<boolean>(false);
 
   const dispatch = useAppDispatch();
-  const { selectedAnswers } = useAppSelector((state) => state.questions);
+  const {selectedAnswers} = useAppSelector((state) => state.questions);
   const findItem = rightAnswers?.find((item) => item === title);
 
-  const selectAnswer = useCallback(() => {
+  const selectAnswer = () => {
     setSelected(!selected);
     dispatch(setSelectAnswer(title));
-  },[]);
+  }
 
   useEffect(() => {
     if (!answered) {
@@ -43,16 +43,16 @@ const Answer: FC<AnswerProps> = ({ title, rightAnswers, answered }) => {
         disabled={answered}
         onClick={() => selectAnswer()}
         className={clsx(
-          { '!hover:none': answered },
-          { '!bg-red-600': wrongIcon },
-          { 'bg-orange-500 text-white': selected },
-          { 'animate !bg-emerald-700 text-white': findItem && answered },
+          {'!hover:none': answered},
+          {'!bg-red-600': wrongIcon},
+          {'bg-orange-500 text-white': selected},
+          {'animate !bg-emerald-700 text-white': findItem && answered},
           ' mb-4 flex min-h-[56px] w-full items-center justify-between gap-x-2 rounded-lg border px-3 py-2 text-left transition-colors enabled:hover:bg-purple-500 enabled:hover:text-white md:px-6 md:py-4'
         )}
       >
         {title}
-        {selected && !wrongIcon && <IconArrow />}
-        {selected && wrongIcon && <IconWrong />}
+        {selected && !wrongIcon && <IconArrow/>}
+        {selected && wrongIcon && <IconWrong/>}
         {}
       </button>
     </li>
